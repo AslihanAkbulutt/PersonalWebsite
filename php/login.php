568a420b962e6ef6efabbfdf1bf17c679eceefb7
AslihanAkbulutt <82841457+AslihanAkbulutt@users.noreply.github.com>
2021-05-06 22:05:32 +0300

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,26 @@
+<?php
+
+error_reporting(0);
+
+if(isset($_POST['login'])){
+    $kulAd="g201210080@sakarya.edu.tr";
+    $sifre="g201210080";
+    $kullaniciAdi=$_POST["kullaniciAdi"];
+    $parola=$_POST["parola"];
+    if($kullaniciAdi!="" && $parola!=""){
+        if($kullaniciAdi==$kulAd && $parola==$sifre){
+            echo "Hoşgeldiniz <br><hr>";
+            echo "$parola";
+        }
+        else{
+            header("Location:../html/login.html");
+        }
+    }
+    else{
+        header("Location:../html/login.html");
+    }
+}
+else {
+    header("Location:../html/login.html");
+}
+?>
