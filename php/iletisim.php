28f2e21225a93b3cb8ada78ced9c7a1aa06162ff
AslihanAkbulutt <82841457+AslihanAkbulutt@users.noreply.github.com>
2021-05-21 22:09:55 +0300

    son hali eklendi.

--- php/iletisim.php
+++ php/iletisim.php
@@ -9,24 +9,24 @@ if(isset($_POST['gonder'])){
     $email=$_POST["email"];
     $cinsiyet=$_POST["cinsiyet"];
     $konu=$_POST["konu"];
-    $diller=$_POST["dil"];
+    $dil=$_POST["dil"];
     $mesaj=$_POST["mesaj"];
-    if($ad!="" && $soyad!="" && $email!="" && $cinsiyet!="" && $konu!="" && $diller!="" && $mesaj!=""){
+    if($ad!="" && $soyad!="" && $email!="" && $cinsiyet!="" && $konu!="" && $dil!="" && $mesaj!=""){
         echo "Ad Soyad : $ad $soyad <br>";
         echo "E-Mail : $email <br>";
         echo "Cinsiyet : $cinsiyet <br>";
         echo "Konu : $konu <br>";
         echo "Diller : ";
-        foreach ($diller as $key => $value) {
+        foreach ($dil as $key => $value) {
             echo "$value / ";
         }
         echo "<br>Mesaj : $mesaj";
     }
     else{
-        header("Location:../html/iletisim.html");
+        header("Location:../iletisim.html");
     }
 }
 else {
-    header("Location:../html/iletisim.html");
+    header("Location:../iletisim.html");
 }
 ?>
